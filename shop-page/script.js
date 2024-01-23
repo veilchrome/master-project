6512d177997e97806f497237c2d2f5b965571dae
Selamet S <keeita768900@gmail.com>
2024-01-23 13:19:37 +0700

    #Pengerjaan Anime Shop #Progress 40%

--- shop-page/script.js
+++ shop-page/script.js
@@ -1,32 +1,53 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 1;
+var autoChangeInterval;
 
-// Next/previous controls
+showSlides(slideIndex);
+toggleAuto(); // Start autoplay by default
+
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+	clearInterval(autoChangeInterval);
+	showSlides((slideIndex += n));
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
-  showSlides((slideIndex = n));
+	clearInterval(autoChangeInterval);
+	showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+}
+
+function toggleAuto() {
+	var autoPlayButton = document.querySelector(".auto-play");
+	if (autoChangeInterval) {
+		clearInterval(autoChangeInterval);
+		autoPlayButton.textContent = "Play";
+	} else {
+		autoChangeInterval = setInterval(function () {
+			plusSlides(1);
+		}, 3000); // Change slide every 3 seconds
+		autoPlayButton.textContent = "Pause";
+	}
 }
